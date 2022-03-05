d4e0b3b47463faf1139d2826c8f7781e5ceef097
vdi-student <vdi-student@EDU-WSB.local>
2022-03-05 15:01:43 +0100

    funkcjaWfunkcji

--- cały_program.py
+++ cały_program.py
@@ -1,4 +1,14 @@
 from modół_przewalutowanie import *
-from modół_logika_biznwsowa import *
+from modół_logika_biznesowa import *
 from modół_promocja import *
+
+PLN = 5651
+wiek = 35
+plec = "m"
+kurs =4.50
+
+logika_biznesowa(plec, wiek, przewalutowanie(PLN, kurs))
+
+
+
 
